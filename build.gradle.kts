1e0688970bf739b2965a6342c906ae7ac5cea381
NyCode <nico@nycode.de>
2021-08-10 22:53:11 +0200

    Add dokka plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("multiplatform") version "1.5.21"
     kotlin("plugin.serialization") version "1.5.21"
+    id("org.jetbrains.dokka") version "1.5.0"
     id("com.diffplug.spotless") version "5.14.2"
     id("at.stnwtr.gradle-secrets-plugin") version "1.0.1"
 }
