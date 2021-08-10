3527271b66b8bac1aaab791fbfa76bb1639498ac
NyCode <nico@nycode.de>
2021-08-10 21:19:53 +0200

    Add docs to listRepositoryContributors function

--- src/commonMain/kotlin/repositories/RepositoriesAPI.kt
+++ src/commonMain/kotlin/repositories/RepositoriesAPI.kt
@@ -150,6 +150,26 @@ public value class RepositoriesAPI(private val gitHubClient: GitHubClient) {
             }
         }
 
+    /**
+     * Lists contributors to the specified repository
+     * and sorts them by the number of commits per contributor in descending order.
+     * This endpoint may return information that is a few hours old
+     * because the GitHub REST API v3 caches contributor data to improve performance.
+     * GitHub identifies contributors by author email address.
+     * This endpoint groups contribution counts by GitHub user,
+     * which includes all associated email addresses.
+     * To improve performance, only the first 500 author email addresses in the repository link to GitHub users.
+     * The rest will appear as anonymous contributors without associated GitHub user information.
+     *
+     * Represents [this endpoint](https://docs.github.com/en/rest/reference/repos#list-repository-contributors).
+     *
+     * @param owner the owner of the repository
+     * @param repo the name of the repo
+     * @param includeAnonymousContributors include anonymous contributors in the results
+     * @param builder builder for configuring pagination
+     * @return [List] of [Contributor]s
+     * @throws GitHubRequestException when the request fails
+     */
     public suspend fun listRepositoryContributors(
         owner: String,
         repo: String,
