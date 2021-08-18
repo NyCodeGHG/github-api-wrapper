f6b7673058680302711367159fa1dd6c9516c66a
NyCode <nico@nycode.de>
2021-08-18 16:05:46 +0200

    Add updateStatusChecksProtection function

--- repositories/branches/src/commonMain/kotlin/RepositoryBranchesAPI.kt
+++ repositories/branches/src/commonMain/kotlin/RepositoryBranchesAPI.kt
@@ -417,4 +417,31 @@ public value class RepositoryBranchesAPI(private val gitHubClient: GitHubClient)
         branch: String
     ): StatusChecks =
         gitHubClient.get("repos", owner, repo, "branches", branch, "protection", "required_status_checks")
+
+    /**
+     * Updates the status checks protection of the specified branch of the specified repository.
+     * Protected branches are available in public repositories with GitHub Free and GitHub Free for organizations,
+     * and in public and private repositories with GitHub Pro, GitHub Team, GitHub Enterprise Cloud, and GitHub Enterprise Server.
+     * For more information, see [GitHub's products](https://help.github.com/github/getting-started-with-github/githubs-products) in the GitHub Help documentation.
+     *
+     * Represents [this endpoint](https://docs.github.com/en/rest/reference/repos#update-status-checks-protection).
+     *
+     * @param owner the owner of the repository
+     * @param repo the name of the repo
+     * @param branch the name of the branch
+     * @return the status checks protection of the branch
+     * @throws de.nycode.github.request.GitHubRequestException when the request fails
+     */
+    public suspend fun updateStatusCheckProtection(
+        owner: String,
+        repo: String,
+        branch: String,
+        builder: StatusChecksBuilder.() -> Unit
+    ): StatusChecks =
+        gitHubClient.patch("repos", owner, repo, "branches", branch, "protection", "required_status_checks") {
+            request {
+                contentType(ContentType.Application.Json)
+                body = StatusChecksBuilder().apply(builder)
+            }
+        }
 }
