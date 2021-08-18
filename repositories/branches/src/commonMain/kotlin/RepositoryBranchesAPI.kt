e6048b891b926c264d694e7fabb2bbdac1e1c917
NyCode <nico@nycode.de>
2021-08-18 15:32:37 +0200

    Add deletePullRequestReviewProtection function

--- repositories/branches/src/commonMain/kotlin/RepositoryBranchesAPI.kt
+++ repositories/branches/src/commonMain/kotlin/RepositoryBranchesAPI.kt
@@ -283,4 +283,23 @@ public value class RepositoryBranchesAPI(private val gitHubClient: GitHubClient)
                 body = EditPullRequestReviewsBuilder().apply(builder)
             }
         }
+
+    /**
+     * Deletes the pull request review protection of the specified branch in the specified repository.
+     * Protected branches are available in public repositories with GitHub Free and GitHub Free for organizations,
+     * and in public and private repositories with GitHub Pro, GitHub Team, GitHub Enterprise Cloud, and GitHub Enterprise Server.
+     * For more information, see [GitHub's products](https://help.github.com/github/getting-started-with-github/githubs-products) in the GitHub Help documentation.
+     *
+     * Represents [this endpoint](https://docs.github.com/en/rest/reference/repos#delete-pull-request-review-protection).
+     * @param owner the owner of the repository
+     * @param repo the name of the repo
+     * @param branch the name of the branch
+     * @throws de.nycode.github.request.GitHubRequestException when the request fails
+     */
+    public suspend fun deletePullRequestReviewProtection(
+        owner: String,
+        repo: String,
+        branch: String
+    ): Unit =
+        gitHubClient.delete("repos", owner, repo, "branches", branch, "protection", "required_pull_request_reviews")
 }
