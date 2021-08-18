7ce5944dd9c6cb58c9eb20af1e4f02ceb223b52d
NyCode <nico@nycode.de>
2021-08-18 15:47:04 +0200

    Add deleteCommitSignatureProtection function

--- repositories/branches/src/commonMain/kotlin/RepositoryBranchesAPI.kt
+++ repositories/branches/src/commonMain/kotlin/RepositoryBranchesAPI.kt
@@ -313,6 +313,11 @@ public value class RepositoryBranchesAPI(private val gitHubClient: GitHubClient)
      * and in public and private repositories with GitHub Pro, GitHub Team, GitHub Enterprise Cloud, and GitHub Enterprise Server.
      * For more information, see [GitHub's products](https://help.github.com/github/getting-started-with-github/githubs-products) in the GitHub Help documentation.
      *
+     * When authenticated with admin or owner permissions to the repository,
+     * you can use this endpoint to check whether a branch requires signed commits.
+     * An enabled status of true indicates you must sign commits on this branch.
+     * For more information, see [Signing commits with GPG](https://help.github.com/articles/signing-commits-with-gpg) in GitHub Help.
+     *
      * Represents [this endpoint](https://docs.github.com/en/rest/reference/repos#get-commit-signature-protection).
      *
      * @param owner the owner of the repository
@@ -339,6 +344,10 @@ public value class RepositoryBranchesAPI(private val gitHubClient: GitHubClient)
      * and in public and private repositories with GitHub Pro, GitHub Team, GitHub Enterprise Cloud, and GitHub Enterprise Server.
      * For more information, see [GitHub's products](https://help.github.com/github/getting-started-with-github/githubs-products) in the GitHub Help documentation.
      *
+     * When authenticated with admin or owner permissions to the repository,
+     * you can use this endpoint to require signed commits on a branch.
+     * You must enable branch protection to require signed commits.
+     *
      * Represents [this endpoint](https://docs.github.com/en/rest/reference/repos#create-commit-signature-protection).
      *
      * @param owner the owner of the repository
@@ -354,6 +363,35 @@ public value class RepositoryBranchesAPI(private val gitHubClient: GitHubClient)
         branch: String
     ): UrlEnabledValue =
         gitHubClient.post("repos", owner, repo, "branches", branch, "protection", "required_signatures") {
+            request {
+                preview(Previews.ZzzaxPreview)
+            }
+        }
+
+    /**
+     * Deletes commit signature protection for the specified branch in the specified repository.
+     * Protected branches are available in public repositories with GitHub Free and GitHub Free for organizations,
+     * and in public and private repositories with GitHub Pro, GitHub Team, GitHub Enterprise Cloud, and GitHub Enterprise Server.
+     * For more information, see [GitHub's products](https://help.github.com/github/getting-started-with-github/githubs-products) in the GitHub Help documentation.
+     *
+     * When authenticated with admin or owner permissions to the repository,
+     * you can use this endpoint to disable required signed commits on a branch.
+     * You must enable branch protection to require signed commits.
+     *
+     * Represents [this endpoint](https://docs.github.com/en/rest/reference/repos#delete-commit-signature-protection).
+     *
+     * @param owner the owner of the repository
+     * @param repo the name of the repo
+     * @param branch the name of the branch
+     * @throws de.nycode.github.request.GitHubRequestException when the request fails
+     */
+    @ApiPreview
+    public suspend fun deleteCommitSignatureProtection(
+        owner: String,
+        repo: String,
+        branch: String
+    ): Unit =
+        gitHubClient.delete("repos", owner, repo, "branches", branch, "protection", "required_signatures") {
             request {
                 preview(Previews.ZzzaxPreview)
             }
