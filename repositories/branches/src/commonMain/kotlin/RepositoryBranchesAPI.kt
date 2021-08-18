3bd5990efb27f632912f244d4ee6665d607ed256
NyCode <nico@nycode.de>
2021-08-18 16:14:34 +0200

    Add getStatusCheckContexts function

--- repositories/branches/src/commonMain/kotlin/RepositoryBranchesAPI.kt
+++ repositories/branches/src/commonMain/kotlin/RepositoryBranchesAPI.kt
@@ -465,4 +465,24 @@ public value class RepositoryBranchesAPI(private val gitHubClient: GitHubClient)
     ): Unit =
         gitHubClient.delete("repos", owner, repo, "branches", branch, "protection", "required_status_checks")
 
+    /**
+     * Gets all status check contexts of the specified branch in the specified repository.
+     * Protected branches are available in public repositories with GitHub Free and GitHub Free for organizations,
+     * and in public and private repositories with GitHub Pro, GitHub Team, GitHub Enterprise Cloud, and GitHub Enterprise Server.
+     * For more information, see [GitHub's products](https://help.github.com/github/getting-started-with-github/githubs-products) in the GitHub Help documentation.
+     *
+     * Represents [this endpoint](https://docs.github.com/en/rest/reference/repos#get-all-status-check-contexts).
+     *
+     * @param owner the owner of the repository
+     * @param repo the name of the repo
+     * @param branch the name of the branch
+     * @return [List] containing all check contexts
+     * @throws de.nycode.github.request.GitHubRequestException when the request fails
+     */
+    public suspend fun getStatusCheckContexts(
+        owner: String,
+        repo: String,
+        branch: String
+    ): List<String> =
+        gitHubClient.get("repos", owner, repo, "branches", branch, "protection", "required_status_checks", "contexts")
 }
