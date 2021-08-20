5b63e37abe1d57fda2fed5d1f56e4644c3e5ded2
NyCode <nico@nycode.de>
2021-08-20 18:39:31 +0200

    Add addUserAccessRestrictions function

--- repositories/branches/src/commonMain/kotlin/RepositoryBranchesAPI.kt
+++ repositories/branches/src/commonMain/kotlin/RepositoryBranchesAPI.kt
@@ -876,4 +876,32 @@ public value class RepositoryBranchesAPI(private val gitHubClient: GitHubClient)
         branch: String
     ): List<SimpleUser> =
         gitHubClient.get("repos", owner, repo, "branches", branch, "protection", "restrictions", "users")
+
+    /**
+     * Grants the specified people push access for this branch.
+     * Protected branches are available in public repositories with GitHub Free and GitHub Free for organizations,
+     * and in public and private repositories with GitHub Pro, GitHub Team, GitHub Enterprise Cloud, and GitHub Enterprise Server.
+     * For more information, see [GitHub's products](https://help.github.com/github/getting-started-with-github/githubs-products) in the GitHub Help documentation.
+     *
+     * Represents [this endpoint](https://docs.github.com/en/rest/reference/repos#add-user-access-restrictions).
+     *
+     * @param owner the owner of the repository
+     * @param repo the name of the repo
+     * @param branch the name of the branch
+     * @param users the users to grant push access
+     * @return [SimpleUser]s which are having push access
+     * @throws de.nycode.github.request.GitHubRequestException when the request fails
+     */
+    public suspend fun addUserAccessRestrictions(
+        owner: String,
+        repo: String,
+        branch: String,
+        users: List<String>
+    ): List<SimpleUser> =
+        gitHubClient.post("repos", owner, repo, "branches", branch, "protection", "restrictions", "users") {
+            request {
+                contentType(ContentType.Application.Json)
+                body = mapOf("users" to users)
+            }
+        }
 }
