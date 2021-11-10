234c7c8815c8775b78aea852ac9a490fc8fb344a
NyCode <me@nycode.dev>
2021-11-10 17:03:01 +0100

    Add createDeployKey function

--- repositories/deploy-keys/src/commonMain/kotlin/RepositoryDeployKeysAPI.kt
+++ repositories/deploy-keys/src/commonMain/kotlin/RepositoryDeployKeysAPI.kt
@@ -13,14 +13,21 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+@file:Suppress("NAME_SHADOWING")
 
 package dev.nycode.github.repositories.deploykeys
 
 import dev.nycode.github.GitHubClientImpl
 import dev.nycode.github.repositories.RepositoriesAPI
 import dev.nycode.github.repositories.deploykeys.model.DeployKey
+import dev.nycode.github.repositories.deploykeys.request.CreateDeployKeyRequestBuilder
+import dev.nycode.github.request.post
 import dev.nycode.github.request.simplePaginatedGet
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import kotlinx.coroutines.flow.Flow
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 import kotlin.jvm.JvmInline
 
 public val RepositoriesAPI.deployKeys: RepositoryDeployKeysAPI
@@ -47,6 +54,35 @@ public value class RepositoryDeployKeysAPI(@PublishedApi internal val gitHubClie
     ): Flow<DeployKey> = gitHubClient.simplePaginatedGet("repos", owner, repo, "keys") {
         if (perPage != null) {
             this.perPage = perPage
+        }
+    }
+
+    /**
+     * Creates a new deploy key in a repository.
+     *
+     * Represents [this endpoint](https://docs.github.com/en/rest/reference/repos#create-a-deploy-key).
+     *
+     * @param owner the owner of the repository
+     * @param repo the name of the repo
+     * @param key the contents of the key
+     * @param builder builder for specifying optional parameters
+     * @return the newly created deploy key.
+     */
+    public suspend inline fun createDeployKey(
+        owner: String,
+        repo: String,
+        key: String,
+        builder: CreateDeployKeyRequestBuilder.() -> Unit = {}
+    ): DeployKey {
+        contract {
+            callsInPlace(builder, InvocationKind.EXACTLY_ONCE)
+        }
+        val builder = CreateDeployKeyRequestBuilder(key).apply(builder)
+        return gitHubClient.post("repos", owner, repo, "keys") {
+            request {
+                contentType(ContentType.Application.Json)
+                body = builder
+            }
         }
     }
 }
